f82f67f15fa7eb05673237979d575e6b6a8f422c
YadavNitesh007 <2313244@students.uca.ac.uk>
2025-10-23 15:54:49 +0100

    Initial commit - Temperature Converter PWA

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = `temperature-converter-v1`;
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll([
+      './',
+      './index.html',
+      './converter.js',
+      './converter.css',
+      './manifest.json',
+      './icon512.png'
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+      try {
+        const fetchResponse = await fetch(event.request);
+        cache.put(event.request, fetchResponse.clone());
+        return fetchResponse;
+      } catch (e) {
+        return new Response('Offline', { status: 503 });
+      }
+    }
+  })());
+});
